fb403750abb59d4e02698cd015bc407c0e215cc9
Bitjockey <fastjack@jackpoint.network>
2022-03-07 18:50:21 -0500

    Move rust_cortex code

--- native/src/api.rs
+++ native/src/api.rs
@@ -1,5 +1,10 @@
-extern crate cortex;
+use rust_bert::pipelines::question_answering::{QaInput, QuestionAnsweringModel};
 
 pub fn ask(question: String, context: String) -> String {
-    cortex::api::ask(question, context)
+    let qa_model = QuestionAnsweringModel::new(Default::default());
+    let answers = qa_model
+        .unwrap()
+        .predict(&[QaInput { question, context }], 1, 32);
+    let answer = &answers[0][0].answer;
+    answer.into()
 }
