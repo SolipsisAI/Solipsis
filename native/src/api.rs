960b98109df641f082ca6a4a9401cec3ebc05a4d
Bitjockey <fastjack@jackpoint.network>
2022-03-07 21:08:18 -0500

    Revert "Move rust_cortex code"
    This reverts commit fb403750abb59d4e02698cd015bc407c0e215cc9.

--- native/src/api.rs
+++ native/src/api.rs
@@ -1,10 +1,5 @@
-use rust_bert::pipelines::question_answering::{QaInput, QuestionAnsweringModel};
+extern crate cortex;
 
 pub fn ask(question: String, context: String) -> String {
-    let qa_model = QuestionAnsweringModel::new(Default::default());
-    let answers = qa_model
-        .unwrap()
-        .predict(&[QaInput { question, context }], 1, 32);
-    let answer = &answers[0][0].answer;
-    answer.into()
+    cortex::api::ask(question, context)
 }
