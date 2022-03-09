e1eaef93f230fa188300449e49218deeaa499e62
Bitjockey <fastjack@jackpoint.network>
2022-03-09 03:30:06 -0500

    Copied main() from original

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,43 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() => runApp(const MyApp());
+import 'chat.dart';
+import 'models/chat_message.dart';
+import 'models/chat_user.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+import 'utils.dart';
 
-  static const String _title = 'Flutter Code Sample';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Isar
+  final dir = await getApplicationSupportDirectory();
+  final Isar _isar = await Isar.open(
+      schemas: [ChatMessageSchema, ChatUserSchema], directory: dir.path);
+  final chatMessages = await _isar.chatMessages.where().findAll();
+
+  // Download files
+  final modelDir = await downloadModelFiles("dialogpt-medium");
+
+  runApp(ElizaApp(modelDir: modelDir, isar: _isar, chatMessages: chatMessages));
+}
+
+class ElizaApp extends StatelessWidget {
+  const ElizaApp(
+      {Key? key,
+      required this.modelDir,
+      required this.isar,
+      required this.chatMessages})
+      : super(key: key);
+
+  final Directory modelDir;
+  final Isar isar;
+  final List<ChatMessage> chatMessages;
+
+  static const String _title = 'Eliza';
 
   @override
   Widget build(BuildContext context) {
